bcba04937a26c1b37eb883c4861fb07cfde99385
Keewis <keewis@posteo.de>
2019-09-11 00:42:13 +0200

    add group defines

---
+++ lisp/pytest-core.el
@@ -0,0 +1,16 @@
+;;; pytest-core --- group definitions
+;;; Commentary:
+;;; Code:
+(defgroup pytest nil
+  "Controlling py.test from Emacs"
+  :group 'python)
+
+(defgroup pytest-modes nil
+  "Modes used or provided by pytest."
+  :group 'pytest)
+
+(defgroup pytest-buffers nil
+  "Names for buffers."
+  :group 'pytest)
+(provide 'pytest-core)
+;;; pytest-core.el ends here
