855b8a815568d90d4b7b8fc7a0900c99b7559123
Keewis <keewis@posteo.de>
2020-06-07 23:14:36 +0200

    don't assume we have / need _static

--- doc/conf.py
+++ doc/conf.py
@@ -52,8 +52,3 @@ master_doc = 'index'
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
