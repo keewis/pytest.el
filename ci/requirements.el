7f1df4323dfdda102cad85e65135d2b46b33bda8
Keewis <keewis@posteo.de>
2020-06-15 00:39:07 +0200

    considerably simplify the ci configuration

---
+++ ci/requirements.el
@@ -0,0 +1,36 @@
+;;; requirements.el --- Install dependencies in a ci  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  pytest.el developers
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+
+(setq package-selected-packages '(s project cl))
+
+(package-install-selected-packages)
+
+(provide 'requirements)
+;;; requirements.el ends here
