a94373438ef1ef123944863fb8aaa8f3c59e91a1
Keewis <keewis@posteo.de>
2020-06-18 12:38:44 +0200

    switch the single selector tests to one suite per function

--- tests/test-selectors.el
+++ tests/test-selectors.el
@@ -92,33 +92,38 @@
 
 ;; single selector manipulation
 (describe "manipulation of a single selector"
-  (it "split the selector into a list (pytest--split-selector)"
-    (expect (pytest--split-selector "test_file.py::TestGroup::test_function")
-            :to-equal '("test_file.py" "TestGroup" "test_function"))
+  (describe "a function to split a selector string into a list (pytest--split-selector)"
+    (it "splits a valid selector"
+      (expect (pytest--split-selector "test_file.py::TestGroup::test_function")
+              :to-equal '("test_file.py" "TestGroup" "test_function")))
     ;; TODO: check how this works with invalid data
     )
-  (it "join a selector list into a string (pytest--join-selector)"
-    (expect (pytest--join-selector '("test_file.py" "TestGroup" "test_function"))
-            :to-equal "test_file.py::TestGroup::test_function")
-    ;; TODO: check how this works with invalid data
-    )
-
-  (it "make sure the selector is properly split into components (pytest--normalize-selector)"
-    (expect (pytest--normalize-selector '("test_file.py" "TestGroup::test_function"))
-            :to-equal '("test_file.py" "TestGroup" "test_function"))
+  (describe "a function to join a selector list into a string (pytest--join-selector)"
+    (it "joins a valid selector"
+      (expect (pytest--join-selector '("test_file.py" "TestGroup" "test_function"))
+              :to-equal "test_file.py::TestGroup::test_function"))
     ;; TODO: check how this works with invalid data
     )
 
-  (it "remove the directory component (pytest--strip-directory)"
-    (expect (pytest--strip-directory '("tests/test_file.py" "test_function"))
-            :to-equal '("test_file.py" "test_function"))
+  (describe "a function to normalize a mixed selector list (pytest--normalize-selector)"
+    (it "correctly separates test components"
+      (expect (pytest--normalize-selector '("test_file.py" "TestGroup::test_function"))
+              :to-equal '("test_file.py" "TestGroup" "test_function")))
+    ;; TODO: check how this works with invalid data
+    )
+
+  (describe "a function to remove the directory from the path part (pytest--strip-directory)"
+    (it "removes a single directory"
+      (expect (pytest--strip-directory '("tests/test_file.py" "test_function"))
+              :to-equal '("test_file.py" "test_function")))
     ;; TODO: try to find edge cases. How does this work when the file
     ;; doesn't have a directory component?
     )
 
-  (it "format the selector for use in a buffer title (pytest--format-selector)"
-    (expect (pytest--format-selector '("tests/test_file.py" "TestGroup" "test_function"))
-            :to-equal "file::Group::function")
+  (describe "a function to format the selector for use in a buffer title (pytest--format-selector)"
+    (it "correctly formats a selector with path and group components"
+      (expect (pytest--format-selector '("tests/test_file.py" "TestGroup" "test_function"))
+              :to-equal "file::Group::function"))
     ;; TODO: more tests
     )
 
